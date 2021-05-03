1fa2103462d8a9aecb7ee9e40213afe2a3ce9d2c
Hidnish <hideonishimura@live.it>
2021-05-03 10:54:53 +0000

    cluster added to the map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,19 +1,17 @@
-var map = L.map('map').setView([46.619261, -33.134766], 2);
+var map = L.map('map', {
+    center: [44.619261, -73.134766],
+    zoom: 3,
+});
 
 L.tileLayer(`https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=ROK2DASDw4SDlqEDm9v4`, {
     attribution: `<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>`,
 }).addTo(map);
 
-var planes = [
-    ["Nice Place", 40.785091, -73.134766],
-    ["Cool Place ", 41.084045, -73.874245],
-    ["Lit Place", 40.754932, -73.984012]
-];
+var place1 = L.marker([40.785091, -73.134766]).bindPopup("Place 1"),
+    place2 = L.marker([41.084045, -73.874245]).bindPopup("Place 2"),
+    place3 = L.marker([40.754932, -73.984012]).bindPopup("Place 3");
 
-for (var i = 0; i < planes.length; i++) {
-    marker = new L.marker([planes[i][1], planes[i][2]])
-        .bindPopup(planes[i][0]).openPopup()
-        .addTo(map);
-}
-
+var markers = new L.MarkerClusterGroup();
+markers.addLayer(place1).addLayer(place2).addLayer(place3);
+map.addLayer(markers);
 
