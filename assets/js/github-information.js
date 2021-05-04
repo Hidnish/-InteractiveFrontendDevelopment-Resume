db0f05dee7ca3755045e59245a41ace4355f4b87
Hidnish <hideonishimura@live.it>
2021-05-04 18:17:30 +0000

    Bugs fixed

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,6 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -54,7 +56,7 @@ function fetchGitHubInformation(event) {
         $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
         function(firstResponse, secondResponse) {
-            var userData = firstResponse[0];  //in case of more than one parameter the $.when packs the results into arrays, therefore [0]
+            var userData = firstResponse[0];
             var repoData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
             $("#gh-repo-data").html(repoInformationHTML(repoData));
